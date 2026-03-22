0ad61cab09cf3323aa6de7be6fed080bbaa65ab1
nischhelm <nischhelm@gmail.com>
2026-03-22 17:06:10 +0100

    explicit dependency on fb 1.3.0+

--- src/main/java/wornhorseshoes/WornHorseshoes.java
+++ src/main/java/wornhorseshoes/WornHorseshoes.java
@@ -11,7 +11,12 @@ import wornhorseshoes.handlers.HorseshoeAcquisition;
 import wornhorseshoes.network.NetworkHandler;
 import wornhorseshoes.proxy.IProxy;
 
-@Mod(modid = WornHorseshoes.MODID, version = WornHorseshoes.VERSION, name = WornHorseshoes.NAME, dependencies = "required-after:fermiumbooter")
+@Mod(
+        modid = WornHorseshoes.MODID,
+        version = WornHorseshoes.VERSION,
+        name = WornHorseshoes.NAME,
+        dependencies = "required-after:fermiumbooter@[1.3.0,);"
+)
 public class WornHorseshoes {
     public static final String MODID = "wornhorseshoes";
     public static final String VERSION = "1.0.2.1";
