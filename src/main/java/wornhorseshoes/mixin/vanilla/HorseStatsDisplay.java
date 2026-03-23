88663b81db03873c671deaa4671c705b0bb3a3c4
nischhelm <nischhelm@gmail.com>
2026-03-23 18:29:02 +0100

    slightly modify horse stats display
    still not happy about it

--- src/main/java/wornhorseshoes/mixin/vanilla/HorseStatsDisplay.java
+++ src/main/java/wornhorseshoes/mixin/vanilla/HorseStatsDisplay.java
@@ -14,27 +14,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiScreenHorseInventory.class)
 public abstract class HorseStatsDisplay extends GuiContainer {
-    @Shadow
-    @Final
-    private AbstractHorse horseEntity;
-
     public HorseStatsDisplay(Container container) {
         super(container);
     }
 
-    @Inject(
-            method = "drawGuiContainerForegroundLayer",
-            at = @At("TAIL")
-    )
+    @Shadow @Final private AbstractHorse horseEntity;
+
+    @Inject(method = "drawGuiContainerForegroundLayer", at = @At("TAIL"))
     private void whs_drawHorseStats(int mouseX, int mouseY, CallbackInfo ci){
         double roundedJumpHeight = this.horseEntity.getHorseJumpStrength();
         roundedJumpHeight = -0.6238977 + 2.9998898 * roundedJumpHeight  + 2.8384039 * roundedJumpHeight * roundedJumpHeight; // approximating with a quadratic [0.1 : 0.1 : 1, 1.5] map to [1.125 1.5 2.125 2.875 3.625 4.375 5.25 10.25]
-        roundedJumpHeight = Math.round(roundedJumpHeight * 10) / 10.;
-        this.fontRenderer.drawString(""+roundedJumpHeight, 27, 19, 0x00AAAAAA);
+        this.fontRenderer.drawString("^" + (int) roundedJumpHeight, 27, 19, 0x00AAAAAA);
 
         double roundedSpeed = this.horseEntity.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue() * 43.17; //apparently 1 speed = 43.17 blocks per sec
-        roundedSpeed = Math.round(roundedSpeed * 10) / 10.;
-        String text = roundedSpeed + "";
+        String text = ">" + (int) roundedSpeed;
         this.fontRenderer.drawString(text, 78 - this.fontRenderer.getStringWidth(text), 19, 0x00AAAAAA);
     }
 }
